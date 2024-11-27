1b6345bc8247a381b82ff62a170c436085450196
gemy3731 <mohamedgamalz3731@gmail.com>
2024-11-27 16:08:13 +0200

    feat: replace old API with new backend API

--- src/components/Home/TrendSlider.tsx
+++ src/components/Home/TrendSlider.tsx
@@ -9,21 +9,21 @@ export default function TrendSlider() {
     getData()
   },[])
   const getData = async ()=>{
-    const res = await fetch('https://movies-api14.p.rapidapi.com/home',{
+    const res = await fetch('https://api.themoviedb.org/3/trending/movie/day?language=en-US',{
       headers:{
-        'x-rapidapi-key':'367c6a66c3msh2fa5e8390db2c71p183662jsn89bf9e9130ad',
-        'x-rapidapi-host':'movies-api14.p.rapidapi.com'
+        accept:'application/json',
+        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1N2ExNDAyY2EwYjRmZWEwMmU2MzMyODY3NmYxNmRkNyIsIm5iZiI6MTczMjY1ODEyNS4yNzAxNDMsInN1YiI6IjY3NDIwNDk2N2I4MjVlNjg1YjRlNWVmMCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.U2NeKqF8qmhVMcci3rG5NKwmQ0jykTrLLpnwR9ieRlw'
       }
     })
-    const finalData = await res.json()
-    setData(finalData[0].movies)
-    console.log(finalData[0].movies);
+    const {results} = await res.json()
+    setData(results)
+    console.log(results);
   }
   return (
-    <div className="h-[90vh]">
+    <div className="h-[70vh]">
       <Carousel>
-        {data && data.map((movie)=>(<div key={movie.id} className=" ">
-          <img src={movie.poster_path} className='w-[100%]  object-cover' alt={movie.original_title} />
+        {data && data.map((movie)=>(<div key={movie.id} className="w-full">
+          <img src={`https://image.tmdb.org/t/p/w500${movie.backdrop_path}`} className='w-[100%] object-cover' alt={movie.original_title} />
         </div>))}
       </Carousel>
     </div>
