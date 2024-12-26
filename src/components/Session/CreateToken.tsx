75c1aa91125a44aee2a6e11d51f6a70b235a4d2f
gemy3731 <mohamedgamalz3731@gmail.com>
2024-12-26 18:02:06 +0200

    fix: fix create session

--- src/components/Session/CreateToken.tsx
+++ src/components/Session/CreateToken.tsx
@@ -1,30 +1,36 @@
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import { setToken } from "../../Redux/tokenSlice";
 import { useState } from "react";
+import { useNavigate, useSearchParams } from "react-router-dom";
 
 
 export default function CreateToken() {
     const [isAuth, setIsAuth] = useState(false)
-    const {token} = useSelector((store:any)=>store.tokenReducer)
+    const [searchParams] = useSearchParams()
+    const url_request_token = searchParams.get('request_token')
+    const navigate = useNavigate()
+    // const {token} = useSelector((store:any)=>store.tokenReducer)
     const dispatch = useDispatch()
     const createRequestToken = async ()=>{
         const res = await fetch(
-            `https://api.themoviedb.org/3/authentication/token/new`,
+            'https://api.themoviedb.org/3/authentication/token/new',
             {
               headers: {
                 accept: "application/json",
                 Authorization:
                   "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1N2ExNDAyY2EwYjRmZWEwMmU2MzMyODY3NmYxNmRkNyIsIm5iZiI6MTczMjM3OTc5OC4wMjIsInN1YiI6IjY3NDIwNDk2N2I4MjVlNjg1YjRlNWVmMCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Sa5TY__-QVyFct-xdXvl0cIxfAPQGwmIqjaB_T9l29E",
               },
-              cache: "force-cache",
+              cache: "no-cache",
             }
           );
           const finalRes = await res.json();
+          console.log(finalRes);
+          
           if (finalRes.success) {
-            dispatch(setToken(finalRes.request_token))
+            
             localStorage.setItem('token',finalRes.request_token)
             setIsAuth(true)
-            open(`https://www.themoviedb.org/authenticate/${finalRes.request_token}?redirect_to=http://localhost:5173/session`)
+            open(`https://www.themoviedb.org/authenticate/${finalRes.request_token}?redirect_to=http://localhost:5173/session`,'_self')
           }
           
     }
@@ -35,27 +41,34 @@ export default function CreateToken() {
             'https://api.themoviedb.org/3/authentication/session/new',
             {
                 method: 'POST',
+                cache: "no-cache",
                 headers: {
                   accept: 'application/json',
                   'content-type': 'application/json',
                   Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1N2ExNDAyY2EwYjRmZWEwMmU2MzMyODY3NmYxNmRkNyIsIm5iZiI6MTczMjY1ODEyNS4yNzAxNDMsInN1YiI6IjY3NDIwNDk2N2I4MjVlNjg1YjRlNWVmMCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.U2NeKqF8qmhVMcci3rG5NKwmQ0jykTrLLpnwR9ieRlw'
                 },
-                body: JSON.stringify({request_token: 'fdd44d132a4d849704ff94e2206d6cfc83a3e8ae'})
+                body: JSON.stringify({request_token: request_token})
               }
           );
           const finalRes = await res.json();
           console.log(finalRes);
-          
+          if(finalRes.success){
+            console.log(finalRes);
+            dispatch(setToken(finalRes.session_id))
+            localStorage.setItem('session_id',finalRes.session_id)
+            navigate('/')
+          }
+
     }
     const askForPermission = (request_token:string)=>{
-        open(`https://www.themoviedb.org/authenticate/${request_token}?redirect_to=http://localhost:5173/session`)
+        open(`https://www.themoviedb.org/authenticate/${request_token}?redirect_to=http://localhost:5173/session`,'_self')
     }
   return (
     <div className="max-w-[1440px] mx-auto mt-72 md:mt-0 bg-white p-10  rounded-[20px] shadow-lg">
         <h2 className="font-bold text-[32px] mb-5 text-[#ff5300] ">Create Session</h2>
         <div className="flex flex-col md:flex-row justify-center items-center gap-5">
-        {!isAuth&&<button onClick={createRequestToken} className="bg-[#ff5300] hover:bg-[#ff5500e2] w-fit px-20 py-3 rounded-md text-[22px] font-bold  my-10">Create</button>}
-        {isAuth&&(<>
+        {!isAuth&&localStorage.getItem('token')!=url_request_token&&<button onClick={createRequestToken} className="bg-[#ff5300] hover:bg-[#ff5500e2] w-fit px-20 py-3 rounded-md text-[22px] font-bold  my-10">Create</button>}
+        {localStorage.getItem('token')==url_request_token&&(<>
         <button onClick={()=>{askForPermission(localStorage.getItem('token')||'')}} className="bg-[#ff5300] hover:bg-[#ff5500e2] w-fit px-20 py-3 rounded-md text-[22px] font-bold  my-10">Ask For Permission</button>
         <button onClick={()=>{createSession(localStorage.getItem('token')||'')}} className="bg-[#ff5300] hover:bg-[#ff5500e2] w-fit px-20 py-3 rounded-md text-[22px] font-bold  my-10">Register</button>
         </>)}
